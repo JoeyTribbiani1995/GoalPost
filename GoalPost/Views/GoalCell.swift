d6e9423b478f0ed91f756fe2c7ea5c1b339f62f1
JoeyTribbiani <vophanhongdung@gmail.com>
2018-04-07 12:45:38 +0700

    commit work with goal cell

---
+++ GoalPost/Views/GoalCell.swift
@@ -0,0 +1,32 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Joey Tribbiani on 4/7/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var gooalDescriptionLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func congfiureCell(description : String , type : String , progress : Int ) {
+        goalProgressLbl.text = "\(progress)"
+        goalTypeLbl.text = type
+        gooalDescriptionLbl.text = description
+    }
+
+}
