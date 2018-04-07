664125a0397ce10d464ee7c74b06945dfa051e11
JoeyTribbiani <vophanhongdung@gmail.com>
2018-04-07 14:01:54 +0700

    commit create goal view controller

---
+++ GoalPost/View Controllers/CreateGoalVC.swift
@@ -0,0 +1,35 @@
+//
+//  CreateGoalVC.swift
+//  GoalPost
+//
+//  Created by Joey Tribbiani on 4/7/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func nextBtnPressed(_ sender: UIButton) {
+    }
+   
+    @IBAction func longTermBtnPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func shortTermBtnPressed(_ sender: UIButton) {
+    }
+    
+}
