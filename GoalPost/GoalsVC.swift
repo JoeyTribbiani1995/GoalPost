82c0170248ce5a50852ba8ea339bf57625ababd1
JoeyTribbiani <vophanhongdung@gmail.com>
2018-04-07 11:15:49 +0700

    commit work with GoalsVC

---
+++ GoalPost/GoalsVC.swift
@@ -0,0 +1,26 @@
+//
+//  GoalsVC.swift
+//  GoalPost
+//
+//  Created by Joey Tribbiani on 4/7/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func addGoalBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
